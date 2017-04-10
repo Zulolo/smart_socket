25a24dd2a1a23d1c88a8265f25d04385d3211c20
zulolo <dodoleon@sina.com>
2017-04-10 21:08:25 +0800

    First commit for internet branch

--- app/user/.output/eagle/debug/obj/user_esp_platform.d
+++ app/user/.output/eagle/debug/obj/user_esp_platform.d
@@ -1,16 +1,16 @@
 .output/eagle/debug/obj/user_esp_platform.o .output/eagle/debug/obj/user_esp_platform.d : user_esp_platform.c \
  /home/zulolo/ESP8266_RTOS_SDK/include/espressif/esp_common.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/espressif/c_types.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stdint.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/stdint.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stdint.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/stdint.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
  /home/zulolo/ESP8266_RTOS_SDK/extra_include/xtensa/config/core-isa.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stdbool.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stddef.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stdbool.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stddef.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/espressif/esp_libc.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/espressif/esp_misc.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/lwip/lwip/ip_addr.h \
@@ -20,17 +20,17 @@
  /home/zulolo/ESP8266_RTOS_SDK/include/lwip/lwip/arch.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/lwip/arch/cc.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/espressif/c_types.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/stdio.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stdarg.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/reent.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/_types.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_types.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_default_types.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/lock.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/types.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/types.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/stdio.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/stdio.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stdarg.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/reent.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/_types.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_types.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_default_types.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/lock.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/types.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/types.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/stdio.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/lwip/lwip/def.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/lwip/ipv4/lwip/ip4_addr.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/lwip/ipv6/lwip/ip6_addr.h \
@@ -44,12 +44,12 @@
  /home/zulolo/ESP8266_RTOS_SDK/include/espressif/esp_spiffs.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/spiffs/spiffs.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/spiffs/spiffs_config.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/stdlib.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/stdlib.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/alloca.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/string.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/cdefs.h \
- /home/zulolo/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/string.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/stdlib.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/stdlib.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/alloca.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/string.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/cdefs.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/string.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/freertos/FreeRTOS.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/freertos/projdefs.h \
  /home/zulolo/ESP8266_RTOS_SDK/include/freertos/FreeRTOSConfig.h \
